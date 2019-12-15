2f9e9160b4b3900a856a4b248b6c2b86136dceb4
gnaps <lorenzo.astengo@gmail.com>
2019-12-15 14:53:49 +0100

    Added connections control in login and registration, used encrypted password in db and modified code in order to use them

--- login.php
+++ login.php
@@ -6,19 +6,39 @@ require_once('db/mysql_credentials.php');
 // Add session control, header, ...
 // Open DBMS Server connection
 $con = new mysqli($mysql_host,$mysql_user,$mysql_pass,$mysql_db);
+if(mysqli_connect_errno($con)){
+    echo "Failed to connect to MySql: " . mysqli_connect_error($con);
+}
 
 // Get credentials from $_POST['email'] and $_POST['pass']
 // but do it IN A SECURE WAY
-$email = trim($_POST["email"]); // replace null with $_POST and sanitization
-$pass = trim($_POST["pass"]); // replace null with $_POST and sanitization
+$email=filter_var(trim($_POST['email']), FILTER_VALIDATE_EMAIL); // replace null with $_POST and sanitization
+if($email !== false)
+    $email=mysqli_real_escape_string($con,$email);
+$password=mysqli_real_escape_string($con,trim($_POST['password'])); // replace null with $_POST and sanitization
 
 function login($email, $pass, $db_connection) {
     // TODO: login logic here
-    $query = "SELECT * FROM '".$users."' WHERE ID='".$ID."' AND PASS='".$PASSW."'";
+    $query = "SELECT * FROM '".$users."' WHERE EMAIL='".$email."'";
     $res = $con->query($query);
-    // Return logged user
-    $obj = mysqli_fetch_object($res);
-    return $obj->ID;
+    if($res!==false && mysqli_num_rows($res)!== 0){
+        $row=mysqli_fetch_assoc($res);
+        $pswd=$row['PASSWORD'];
+        if(password_verify($password, $pswd)){
+            // Return logged user
+            $obj = mysqli_fetch_object($res);
+            mysqli_free_result($res);
+            return $obj->EMAIL;
+        }
+        else{
+            echo "ERROR: wrong password. <br>";
+        }
+    }
+    else{
+        echo "ERROR: in database check. <br>";
+    }    
+    mysqli_free_result($res);
+    return false;
 }
 
 // Get user from login
--- registration.php
+++ registration.php
@@ -5,23 +5,26 @@ require_once('db/mysql_credentials.php');
 
 // Open DBMS Server connection
 $con = new mysqli($mysql_host,$mysql_user,$mysql_pass,$mysql_db);
+if(mysqli_connect_errno($con)){
+    echo "Failed to connect to MySql: " . mysqli_connect_error($con);
+}
 
 // Get values from $_POST, but do it IN A SECURE WAY
-$email = trim($_POST["email"]); // replace null with $_POST and sanitization
-$first_name = trim($_POST["firstname"]); // replace null with $_POST and sanitization
-$last_name = trim($_POST["lastname"]); // replace null with $_POST and sanitization
-$password = trim($_POST["pass"]); // replace null with $_POST and sanitization
-$password_confirm =trim($_POST["confirm"]); // replace null with $_POST and sanitization
+$email=mysqli_real_escape_string($con,$email);  // replace null with $_POST and sanitization
+$first_name=mysqli_real_escape_string($con,trim($_POST['firstname']));  // replace null with $_POST and sanitization
+$last_name=mysqli_real_escape_string($con,trim($_POST['lastname']));    // replace null with $_POST and sanitization
+$password=password_hash(mysqli_real_escape_string($con,trim($_POST['password'])),PASSWORD_DEFAULT); // replace null with $_POST and sanitization
+$password_confirm =mysqli_real_escape_string($con,trim($_POST['confirm'])); // replace null with $_POST and sanitization
 
 // Get additional values from $_POST, but do it IN A SECURE WAY
 // If you have additional values, change functions params accordingly
 
 function insert_user($email, $first_name, $last_name, $password, $password_confirm, $db_connection) {
     // TODO: check if passwords match
-    if($password == $password_confirm){
+    if(password_verify($password_confirm,$password)){
         // TODO: registration logic here
-         $query = "INSERT INTO '".$users."' VALUES ('".$email."','".$password."','".$first_name."', '".$last_name."')";
-         $res = $con->query($query);
+        $query = "INSERT INTO '".$users."' VALUES ('".$email."','".$password."','".$first_name."', '".$last_name."')";
+        $res = $con->query($query);
         // Return if the registration was successful
         if($res) return true;
     }
