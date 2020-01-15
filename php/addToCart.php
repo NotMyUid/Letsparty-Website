6a5432f5f4d2911e1981159b61b8a16e25029c5f
LorenzoAstengo <lorenzo.astengo@gmail.com>
2020-01-15 15:08:39 +0100

    Corrections on addToCart

--- php/addToCart.php
+++ php/addToCart.php
@@ -6,6 +6,16 @@
     else{
         header("../html/Event.php");
     }
+    require_once('../db/mysql_credentials.php');
+    $con = new mysqli($mysql_host,$mysql_user,$mysql_pass,$mysql_db, $mysql_port);
+    $query="SELECT * FROM Events WHERE ID='".$ID."'";
+    $res = $con->query($query);
+    if ($res->num_rows > 0) {
+        $event = $res->fetch_assoc();
+        mysqli_free_result($res); 
+    }else{
+      echo("Unexpected error <br>");
+    }
     if(!isset($_SESSION["cart"]))
         $_SESSION['cart'] = array();
     if(!isset($_SESSION["cart"][$ID]))
