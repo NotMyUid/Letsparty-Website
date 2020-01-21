c3d1baa7a0b6edeb3ec10508bbc76c78d14be06c
LorenzoAstengo <lorenzo.astengo@gmail.com>
2020-01-21 16:54:49 +0100

    little mods on update profile

--- html/UpdateProfile.php
+++ html/UpdateProfile.php
@@ -102,7 +102,7 @@
         
         </div>
 
-        <div class="footer">
+        <div class="footer2">
             <h3>Contact us: <a href="mailto:info@letsparty.staff.com">info@letsparty.staff.com</a></h3>
         </div>      
 
